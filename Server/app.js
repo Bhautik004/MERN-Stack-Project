cb6269562738e2ec3a90081a1d8fa12e525b046c
Bhautik004 <2000bhautikpatel@gmail.com>
2021-12-16 12:27:23 +0530

    middleware Done

--- Server/app.js
+++ Server/app.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const middleware = (req,res,next)=>{
+    console.log("hello my middleware");
+    next();
+}
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/about', (req, res) => res.send('Hello World about us!'))
+app.get('/about', middleware, (req, res) => res.send('Hello World about us!'))
 app.get('/contact', (req, res) => res.send('Hello World contact !'))
 
 
